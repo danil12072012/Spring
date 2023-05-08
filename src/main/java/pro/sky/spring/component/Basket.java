7e3f79307dfd54bb7d25836e208df3a2a993c54b
mycomputer <Danil12.07.2012@gail.com>
2023-05-08 17:30:36 +0300

    Tasks

---
+++ src/main/java/pro/sky/spring/component/Basket.java
@@ -0,0 +1,19 @@
+package pro.sky.spring.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.*;
+@Component
+@SessionScope
+public class Basket {
+    private final Set<Integer> ids = new HashSet<>();
+
+    public void add(Set<Integer> ids) {
+        this.ids.addAll(ids);
+    }
+
+    public Set<Integer> get() {
+        return Collections.unmodifiableSet(ids);
+    }
+}
